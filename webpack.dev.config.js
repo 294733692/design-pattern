7094579f862f972694468b7e048ea3c68452402a
余云超 <18113406084@163.com>
2021-04-11 22:09:18 +0800

    first commit

---
+++ webpack.dev.config.js
@@ -0,0 +1,28 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+module.exports = {
+  entry: "./src/index",
+  output: {
+    path: __dirname,
+    filename: "./release/bundle.js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "index.html"),
+    }),
+  ],
+  module:{
+    rules:[
+      {
+        test: /.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "./release"), // 设置根目录
+    open: true, // 自动打开浏览器
+    port: 8000, // 设置端口号
+  },
+};
